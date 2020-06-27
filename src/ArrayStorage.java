215142a01e3149591215bb21108d7bfda1cbaddd
Tony <shatiloaa@gmail.com>
2020-06-28 01:52:07 +0300

    HW with Streams

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,59 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int counter = 0;
 
     void clear() {
+        for (int i = 0; i < counter; i++) {
+            storage[i] = null;
+        }
+        counter = 0;
     }
 
     void save(Resume r) {
+        if (Arrays.stream(storage).filter(Objects::nonNull).noneMatch(resume -> resume.uuid.equals(r.uuid))) {
+            storage[counter++] = r;
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume != null && resume.uuid.equals(uuid)) {
+                return resume;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+
+        int indexDelObj = 0;
+
+        for (Resume r : storage) {
+            if (!(r.uuid.equals(uuid))) {
+                indexDelObj++;
+            } else {
+                for (int i = indexDelObj; i < counter; i++) {
+                    storage[i] = storage[i + 1];
+                }
+
+                return;
+            }
+
+        }
+        storage[--counter] = null;
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.stream(storage).filter(Objects::nonNull).toArray(Resume[]::new);
     }
 
     int size() {
-        return 0;
+        return counter;
     }
 }
