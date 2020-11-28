6a6620b241fc1603e9d000b0c9e8dac681d8f120
Tony <shatiloaa@gmail.com>
2020-11-28 17:42:53 +0300

    HW12 main streams fixed

--- src/ru/javawebinar/basejava/MainStreams.java
+++ src/ru/javawebinar/basejava/MainStreams.java
@@ -2,7 +2,7 @@ package ru.javawebinar.basejava;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
@@ -11,29 +11,26 @@ import static java.util.Arrays.stream;
 public class MainStreams {
 
     public static void main(String[] args) {
-        System.out.println(minValue(new int[]{1,2,3,3,2,3}));
-        System.out.println(minValue(new int[]{9,8}));
+
+        System.out.println(minValue(new int[]{1, 2, 3, 3, 2, 3}));
+        System.out.println(minValue(new int[]{9, 8}));
+
         System.out.println(oddOrEven(Arrays.asList(1, 2, 3, 3, 2, 3)));
         System.out.println(oddOrEven(Arrays.asList(1, 2, 3, 3, 2, 4)));
     }
 
     private static int minValue(int[] values) {
-        int[] digits = stream(values).sorted().distinct().toArray();
-        AtomicInteger size = new AtomicInteger(digits.length);
-        return stream(digits).reduce(0,
-                (x, y) -> x + (y * exponentiationOfTen(size.decrementAndGet())));
-    }
-
-    private static int exponentiationOfTen(int e) {
-        return (int) Math.pow(10, e);
+        return stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0,
+                        (x, y) -> x * 10 + y);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        int sum = integers.stream().reduce(0, (x, y) -> x + y);
-        if (sum % 2 == 0) {
-            return integers.stream().filter(a -> a % 2 != 0).collect(Collectors.toList());
-        } else {
-            return integers.stream().filter(a -> a % 2 == 0).collect(Collectors.toList());
-        }
+        int sum = integers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        return integers.stream().filter(sum % 2 == 0 ? a -> a % 2 != 0 : a -> a % 2 == 0).collect(Collectors.toList());
     }
 }
